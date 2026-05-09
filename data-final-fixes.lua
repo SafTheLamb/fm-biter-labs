1d57207a9311221df6606407c29ff9b885602b40
Sapphira Riza <safthelamb@gmail.com>
2026-05-09 00:34:49 -0700

    Replace labs with science altars everywhere

---
+++ data-final-fixes.lua
@@ -0,0 +1,19 @@
+local ftech = require("__fdsl__.lib.technology")
+local frep = require("__fdsl__.lib.recipe")
+
+local techs = ftech.find_by_unlock("lab")
+for _,tech_name in pairs(techs) do
+	ftech.replace_unlock(tech_name, "lab", "science-altar")
+	data.raw.recipe["science-altar"].enabled = false
+end
+
+for _,tech in pairs(data.raw.technology) do
+	if tech.research_trigger and tech.research_trigger.item == "lab" then
+		tech.research_trigger.item = "science-altar"
+	end
+end
+
+local recipes = frep.find_by_ingredient("lab")
+for _,recipe_name in pairs(recipes) do
+	frep.replace_ingredient(recipe_name, "lab", "science-altar")
+end
